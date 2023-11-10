ef4dba408471fc428cc1cc46fc71a45d7b041b2b
Alejandro <dtrujillol@miumg.edu.gt>
2023-11-10 12:47:46 -0600

    cambio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -85,7 +85,7 @@
 
 @section('css')
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .card {
             display: flex;
@@ -107,6 +107,6 @@
 @stop
 
 @section('js')
-    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 @stop
